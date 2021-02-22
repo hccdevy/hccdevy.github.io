6afa60ef006092a3a4e5ff7cd6a39150646bed5f
Tommy <24283779+tnwin@users.noreply.github.com>
2021-02-22 02:34:04 -0800

    added automatic github changelog

---
+++ index.js
@@ -0,0 +1,24 @@
+function setGitLogs() {
+  fetch("https://api.github.com/repos/hccdevy/hccdevy.github.io/commits")
+    .then((response) => response.json())
+    .then((commits) => {
+      let git = document.querySelector(".git");
+      git.innerHTML = "";
+
+      for (let i = 0; i < 10; i++) {
+        let modified = new Date(commits[i].commit.committer.date)
+          .toString()
+          .slice(0, 25);
+        let message = commits[i].commit.message;
+        let url = commits[i].html_url;
+
+        let commit = document.createElement("li");
+        commit.innerHTML = `<p>${modified}:
+        <a href=${url} target="_blank">${message}</a></p>`;
+        console.log(commit);
+        git.appendChild(commit);
+      }
+    });
+}
+
+setGitLogs();
